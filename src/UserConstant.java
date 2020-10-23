cd5213c49352ff9d84274180fd1e86986dc0b670
Haoran Yin <70166830+haoran-y@users.noreply.github.com>
2020-10-23 18:02:57 -0400

    moved user data to data folder

--- src/UserConstant.java
+++ src/UserConstant.java
@@ -1,6 +1,6 @@
 public class UserConstant {
 
-    protected static final String USER_FILE_NAME = "user.json";
+    protected static final String USER_FILE_NAME = "data/user.json";
     protected static final String USER_USERNAME = "username";
     protected static final String USER_PASSWORD = "password";
     protected static final String USER_IS_MANAGER = "isManager";
--- src/test.java
+++ src/test.java
@@ -1,6 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Users test = Users.getInstance();
         test.addUser("one", "1", true);
         test.addUser("two", "2", false);
